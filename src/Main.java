3ff75001c594f08d52100afa6ad8efde25515a16
smgKV878 <kliment.antonov@gmail.com>
2023-12-13 15:45:53 +0200

    updates

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,18 @@
 public class Main {
 
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+
     public static void main(String[] args) {
         Referat referat = new Referat();
         referat.name = new Author();
         referat.theme = "Theme";
         referat.pages = 10;
 
-        referat.super.clone();
-
         for(int i=0; i<3; i++) {
+            referat.super.clone();
             referat.Author().name.clone();
         }
     }
